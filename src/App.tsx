20a9110e2ce547e5a2a5e4e124ef5aa53c8071b2
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-12-05 10:42:40 -0300

    feat: add localStorage cache

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from './App.module.css'
 import './styles/global.css'
 import { NewTaskForm } from './components/NewTaskForm'
 import { Tasks } from './components/Tasks'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export interface ITask {
   id: number
@@ -12,7 +12,29 @@ export interface ITask {
 }
 
 export function App() {
-  const [tasks, setTasks] = useState<ITask[]>([])
+  const [tasks, setTasks] = useState<ITask[]>(() => {
+    const localStorageTasks = localStorage.getItem('@todo-list/tasks')
+
+    if (localStorageTasks) {
+      return JSON.parse(localStorageTasks)
+    }
+
+    return []
+  })
+
+  const prevTasksRef = useRef<ITask[]>([])
+
+  useEffect(() => {
+    prevTasksRef.current = tasks
+  })
+
+  const tasksPreviousValue = prevTasksRef.current ?? tasks
+
+  useEffect(() => {
+    if (tasksPreviousValue !== tasks) {
+      localStorage.setItem('@todo-list/tasks', JSON.stringify(tasks))
+    }
+  }, [tasks, tasksPreviousValue])
 
   function createNewTask(input: string) {
     const newTask = {
